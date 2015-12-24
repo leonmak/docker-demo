6110ab9238d81a43e168779c7248718c9517e40a
leon mak <leonmakansheng@gmail.com>
2015-12-24 21:03:37 +0800

    test break

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 app.set('views', 'views');
 app.set('view engine', 'jade');
 
-app.get('/f', function(req, res) {
+app.get('/', function(req, res) {
     res.render('home', {
   });
 });
